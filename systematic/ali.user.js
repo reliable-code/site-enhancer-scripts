49c38f5a7332a3d73f7a2ad90e57fe35d388ddd9
Stepan <stepan.saitov@gmail.com>
2025-09-17 00:27:45 +0500

    Update systematic user scripts (auto-publish)

--- systematic/ali.user.js
+++ systematic/ali.user.js
@@ -5,7 +5,7 @@
 // @grant        GM_getValue
 // @match        https://aliexpress.ru/*
 // @namespace    https://github.com/reliable-code/site-enhancer-scripts
-// @version      1.0.75805044
+// @version      1.0.75805085
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.ru
 // @author       reliable-code
 // @downloadURL  https://raw.githubusercontent.com/reliable-code/site-enhancer-scripts/main/systematic/ali.user.js

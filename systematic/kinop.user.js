7d1c4555eb491805c31b82a55b10e9035207f587
Stepan <stepan.saitov@gmail.com>
2026-06-24 04:44:36 +0500

    Update systematic user scripts (auto-publish)

--- systematic/kinop.user.js
+++ systematic/kinop.user.js
@@ -6,7 +6,7 @@
 // @grant        GM_getValue
 // @match        https://www.kinopoisk.ru/*
 // @namespace    https://github.com/reliable-code/site-enhancer-scripts
-// @version      1.0.78135828
+// @version      1.0.78225827
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kinopoisk.ru
 // @author       reliable-code
 // @downloadURL  https://raw.githubusercontent.com/reliable-code/site-enhancer-scripts/main/systematic/kinop.user.js
